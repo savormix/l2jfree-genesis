3baa6fb52beb20f9e419116c47fedd38263391b4
nbali <nbalika@gmail.com>
2011-07-21 20:34:12 +0000

    GPL
    git-svn-id: http://svn.l2jfree.com/l2jfree/branches/genesis@8989 6ff00ba9-25cd-4355-987e-099c62bc2628

--- l2jfree-login/src/main/java/com/l2jfree/loginserver/network/client/packets/sendable/LoginFailure.java
+++ l2jfree-login/src/main/java/com/l2jfree/loginserver/network/client/packets/sendable/LoginFailure.java
@@ -5,7 +5,7 @@
  * version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY), without even the implied warranty of MERCHANTABILITY or FITNESS
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  * 
